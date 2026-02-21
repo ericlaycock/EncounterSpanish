ea31b67790e639f09b05a422f8c050b365c5e729
Eric <ericlaycock44@gmail.com>
2026-02-20 16:22:09 -0800

    Fix: Remove duplicate content from migrations 004 and 005

--- migrations/versions/005_add_dialect_and_quiz.py
+++ migrations/versions/005_add_dialect_and_quiz.py
@@ -1,7 +1,7 @@
 """Add dialect and quiz scores to users
 
 Revision ID: 005_dialect_quiz
-Revises: 004_add_notes_column_to_words
+Revises: 004_notes
 Create Date: 2024-02-15 00:00:00.000000
 
 """
@@ -30,68 +30,3 @@ def downgrade() -> None:
     op.drop_column('users', 'vocab_score')
     op.drop_column('users', 'grammar_score')
     op.drop_column('users', 'dialect')
-
-
-Revision ID: 005_dialect_quiz
-Revises: 004_add_notes_column_to_words
-Create Date: 2024-02-15 00:00:00.000000
-
-"""
-from alembic import op
-import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
-
-# revision identifiers, used by Alembic.
-revision = '005_dialect_quiz'
-down_revision = '004_notes'
-branch_labels = None
-depends_on = None
-
-
-def upgrade() -> None:
-    # Add dialect and quiz scores to users table
-    op.add_column('users', sa.Column('dialect', sa.String(), nullable=True))
-    op.add_column('users', sa.Column('grammar_score', sa.String(), nullable=True))
-    op.add_column('users', sa.Column('vocab_score', sa.String(), nullable=True))
-    
-    # Change selected_situation_categories to single category (string instead of array)
-    # We'll keep it as JSONB for now but change the API to accept single category
-
-
-def downgrade() -> None:
-    op.drop_column('users', 'vocab_score')
-    op.drop_column('users', 'grammar_score')
-    op.drop_column('users', 'dialect')
-
-
-Revision ID: 005_dialect_quiz
-Revises: 004_add_notes_column_to_words
-Create Date: 2024-02-15 00:00:00.000000
-
-"""
-from alembic import op
-import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
-
-# revision identifiers, used by Alembic.
-revision = '005_dialect_quiz'
-down_revision = '004_notes'
-branch_labels = None
-depends_on = None
-
-
-def upgrade() -> None:
-    # Add dialect and quiz scores to users table
-    op.add_column('users', sa.Column('dialect', sa.String(), nullable=True))
-    op.add_column('users', sa.Column('grammar_score', sa.String(), nullable=True))
-    op.add_column('users', sa.Column('vocab_score', sa.String(), nullable=True))
-    
-    # Change selected_situation_categories to single category (string instead of array)
-    # We'll keep it as JSONB for now but change the API to accept single category
-
-
-def downgrade() -> None:
-    op.drop_column('users', 'vocab_score')
-    op.drop_column('users', 'grammar_score')
-    op.drop_column('users', 'dialect')
-
